0c1502311c4f173b1f3b2abc0e4b7443e2f0e162
Saida <argelon1@gmail.com>
2024-07-11 11:05:52 +0300

    constants 2

--- src/outputs.py
+++ src/outputs.py
@@ -4,14 +4,13 @@ import logging
 
 from prettytable import PrettyTable
 
-from constants import (BASE_DIR, CSV_OUTPUT, DATETIME_FORMAT, PRETTY_OUTPUT)
+from constants import BASE_DIR, DATETIME_FORMAT
 
 
 def control_output(results, cli_args):
-
     OUTPUT_ACTIONS = {
-        PRETTY_OUTPUT: pretty_output,
-        CSV_OUTPUT: file_output,
+        'pretty': pretty_output,
+        'file': file_output,
     }
     output = cli_args.output
     selected_output_function = OUTPUT_ACTIONS.get(output, default_output)
@@ -35,7 +34,7 @@ def pretty_output(results):
 
 
 def file_output(results, cli_args):
-    results_dir = BASE_DIR / 'results'
+    results_dir = BASE_DIR/'results'
     results_dir.mkdir(exist_ok=True)
 
     parse_mode = cli_args.mode
@@ -46,5 +45,6 @@ def file_output(results, cli_args):
 
     with open(file_path, 'w', encoding='utf-8', newline='') as f:
         writer = csv.writer(f, dialect='unix', delimiter=' ', quotechar='|')
+        writer.writerow(['Spam', 'Baked Beans', 'hjkl'])
         writer.writerows(results)
     logging.info(f'Файл с результатами был сохранён: {file_path}')
